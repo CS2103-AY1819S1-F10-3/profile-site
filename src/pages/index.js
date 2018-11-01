d81399d638bb3290f4c37e8ca008e9529b9b2a5b
liaujianjie <liaujianjie@gmail.com>
2018-11-01 21:25:28 +0800

    Handle malformed URL params gracefully

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,49 +7,84 @@ const Entity = {
   SERVICE_PROVIDER: 'SERVICE_PROVIDER',
 }
 
+const Header = props => (
+  <div
+    style={{
+      background: '#e91e63',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      {props.children}
+    </div>
+  </div>
+)
+
+const Body = props => (
+  <div
+    style={{
+      padding: '0px 1.0875rem 1.45rem',
+    }}
+  >
+    {props.children}
+  </div>
+)
+
+const InvalidFormat = props => {
+  return (
+    <Layout>
+      <Header>
+        <h1 style={{ margin: 0, color: '#ffffff' }}>
+          Invalid URL query format
+        </h1>
+      </Header>
+      <Body>{props.children}</Body>
+    </Layout>
+  )
+}
+
 const IndexPage = props => {
-  const entity = queryString.parse(props.location.search)
-  entity.services = entity.services.split(',').map(unprocessedService => {
-    const splits = unprocessedService.split(':')
-    return {
-      type: splits[0],
-      cost: splits[1],
-    }
-  })
+  let entity
+  let splits
+  try {
+    entity = queryString.parse(props.location.search)
+    entity.services = entity.services.split(',').map(unprocessedService => {
+      splits = unprocessedService.split(':')
+      return {
+        type: splits[0],
+        cost: splits[1],
+      }
+    })
+  } catch (error) {
+    return (
+      <InvalidFormat>
+        <pre>{error.stack}</pre>
+      </InvalidFormat>
+    )
+  }
 
   return (
     <Layout>
-      <div
-        style={{
-          background: '#e91e63',
-          marginBottom: '1.45rem',
-        }}
-      >
-        <div
+      <Header>
+        <h1 style={{ margin: 0, color: '#ffffff' }}>{entity.name}</h1>
+        <code
           style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '1.45rem 1.0875rem',
+            paddingLeft: '0.5em',
+            paddingRight: '0.5em',
+            background: '#f7d2e0',
+            color: '#e91e63',
           }}
         >
-          <h1 style={{ margin: 0, color: '#ffffff' }}>{entity.name}</h1>
-          <code
-            style={{
-              paddingLeft: '0.5em',
-              paddingRight: '0.5em',
-              background: '#f7d2e0',
-              color: '#e91e63',
-            }}
-          >
-            {entity.type.toLowerCase()}#{entity.id}
-          </code>
-        </div>
-      </div>
-      <div
-        style={{
-          padding: '0px 1.0875rem 1.45rem',
-        }}
-      >
+          {entity.type.toLowerCase()}#{entity.id}
+        </code>
+      </Header>
+      <Body>
         <label>Residential address</label>
         <address>{`123 Fake Rd \n#12-34\nSingapore 123123`}</address>
 
@@ -65,7 +100,7 @@ const IndexPage = props => {
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
         <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }} />
-      </div>
+      </Body>
     </Layout>
   )
 }
